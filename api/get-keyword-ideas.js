4c6fe8a6371ca7c225ec2ddc6dfe21e7494910da
Ehab Mansour <mansehab990@gmail.com>
2025-06-26 22:30:53 +0300

    d

--- api/get-keyword-ideas.js
+++ api/get-keyword-ideas.js
@@ -38,8 +38,7 @@ module.exports = async (req, res) => {
   try {
     const response = await axios({
       method: "post",
-      url: "https://api.dataforseo.com/v3/keywords_data/google/search_volume/live",
-      // url: "https://api.dataforseo.com/v3/dataforseo_labs/google/keyword_ideas/live",
+      url: "https://api.dataforseo.com/v3/dataforseo_labs/google/keyword_ideas/live",
       auth: {
         username: API_USERNAME,
         password: API_PASSWORD,
