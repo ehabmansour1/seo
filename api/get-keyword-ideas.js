f42df310e1073c6cdf4cf158cc921e99219c0c5d
Ehab Mansour <mansehab990@gmail.com>
2025-06-26 21:23:36 +0300

    d

--- api/get-keyword-ideas.js
+++ api/get-keyword-ideas.js
@@ -1,6 +1,16 @@
 const axios = require("axios");
 
 module.exports = async (req, res) => {
+  // Set CORS headers
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // Handle preflight OPTIONS request
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
@@ -21,7 +31,7 @@ module.exports = async (req, res) => {
       keywords,
       location_code,
       language_code,
-      limit: 1,
+      limit: 3,
     },
   ];
 
